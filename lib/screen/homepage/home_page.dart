86884ffce29a75ab7465a4a86fd319d38a48818f
SalmanMohammed2001 <salmannisthar123@gmail.com>
2024-04-13 01:06:55 +0530

    Main Heading implementation

--- lib/screen/homepage/home_page.dart
+++ lib/screen/homepage/home_page.dart
@@ -12,11 +12,34 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade300,
+      backgroundColor: Colors.grey.shade200,
       body: const SafeArea(
         child: Padding(
           padding: EdgeInsets.all(12.0),
-          child: CustomAppBar(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomAppBar(),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Good Morning Alex",
+                style: TextStyle(color: Colors.grey, fontSize: 17),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Find Your\nCreative Job",
+                style: TextStyle(
+                  fontSize: 32,
+                  height: 0.9,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
