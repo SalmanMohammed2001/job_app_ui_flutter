a66682148a3f939bb75015f39ae125cf1ce0bfd2
SalmanMohammed2001 <salmannisthar123@gmail.com>
2024-04-13 01:51:57 +0530

    Search Bar  implementation

--- lib/screen/homepage/home_page.dart
+++ lib/screen/homepage/home_page.dart
@@ -11,33 +11,70 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+
     return Scaffold(
-      backgroundColor: Colors.grey.shade200,
-      body: const SafeArea(
+      backgroundColor: Colors.grey.shade300,
+      body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(12.0),
+          padding: const EdgeInsets.all(12.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              CustomAppBar(),
-              SizedBox(
+              const CustomAppBar(),
+              const SizedBox(
                 height: 10,
               ),
-              Text(
+              const Text(
                 "Good Morning Alex",
                 style: TextStyle(color: Colors.grey, fontSize: 17),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
-              Text(
+              const Text(
                 "Find Your\nCreative Job",
                 style: TextStyle(
                   fontSize: 32,
                   height: 0.9,
                   fontWeight: FontWeight.bold,
                 ),
-              )
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: size.width * 0.75,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: UnderlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        hintText: "search For Jobs",
+                        fillColor: Colors.white,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: 55,
+                    height: 55,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(12)),
+                    child: const Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
             ],
           ),
         ),
