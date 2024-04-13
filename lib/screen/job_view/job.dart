b8dd388edf1bc9e49528db5c04ae79ace9eb9eeb
SalmanMohammed2001 <salmannisthar123@gmail.com>
2024-04-13 23:29:48 +0530

    JobPage Banner BackButton implementation

--- lib/screen/job_view/job.dart
+++ lib/screen/job_view/job.dart
@@ -23,6 +23,21 @@ class _JobViewPageState extends State<JobViewPage> {
               image: NetworkImage(
                   "https://freerangestock.com/sample/74590/job-interview--illustration-with-copyspace.jpg"),
             )),
+            child: Stack(
+              children: [
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.black.withOpacity(0.5),
+                      child: const Icon(
+                        Icons.arrow_back,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ],
       ),
