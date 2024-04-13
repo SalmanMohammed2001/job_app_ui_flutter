41052793990f29f111139e64bf31ddbca98859bd
SalmanMohammed2001 <salmannisthar123@gmail.com>
2024-04-13 21:56:50 +0530

    Popular Widget add separate

--- lib/screen/homepage/home_page.dart
+++ lib/screen/homepage/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:job_app_ui/screen/homepage/widget/app_bar.dart';
 import 'package:job_app_ui/screen/homepage/widget/category_bar.dart';
 import 'package:job_app_ui/screen/homepage/widget/greetings_text.dart';
+import 'package:job_app_ui/screen/homepage/widget/popular_row.dart';
 import 'package:job_app_ui/screen/homepage/widget/search_bar.dart';
 import 'package:job_app_ui/service/get_deta.dart';
 
@@ -46,120 +47,11 @@ class _HomePageState extends State<HomePage> {
                 const SizedBox(
                   height: 10,
                 ),
-                SingleChildScrollView(
-                  physics: const BouncingScrollPhysics(),
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: List.generate(data.getJobData().length, (index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          width: size.width * 0.75,
-                          height: size.width * 0.4,
-                          padding: const EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                            color: Colors.grey.shade900,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Column(
-                            children: [
-                              Row(
-                                children: [
-                                  Text(
-                                    data.getJobData()[index].title,
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
-                                  const Spacer(),
-                                  Icon(
-                                    Icons.book_online,
-                                    color: Colors.grey.shade700,
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Text(
-                                    data.getJobData()[index].salary,
-                                    style: const TextStyle(
-                                      color: Colors.grey,
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    width: 8,
-                                  ),
-                                  Container(
-                                    padding: const EdgeInsets.all(4),
-                                    decoration: BoxDecoration(
-                                      color: Colors.grey,
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                    child: Text(
-                                      data.getJobData()[index].type,
-                                      style:
-                                          const TextStyle(color: Colors.white),
-                                    ),
-                                  )
-                                ],
-                              ),
-                              const Spacer(),
-                              Row(
-                                children: [
-                                  CircleAvatar(
-                                    radius: 25,
-                                    backgroundImage: NetworkImage(
-                                        data.getJobData()[index].logo),
-                                  ),
-                                  const SizedBox(
-                                    width: 10,
-                                  ),
-                                  Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        data.getJobData()[index].companyName,
-                                        style: const TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      Text(
-                                        data.getJobData()[index].city,
-                                        style: TextStyle(
-                                          color: Colors.grey.shade200,
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w300,
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                  const Spacer(),
-                                  Text(
-                                    data.getJobData()[index].time,
-                                    style: const TextStyle(
-                                      color: Colors.red,
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w300,
-                                    ),
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    }),
-                  ),
-                ),
+                PopularRow(data: data, size: size),
                 const SizedBox(
                   height: 15,
                 ),
-                const CategoryBar(title: "Recents Jobs"),
+                const CategoryBar(title: "Recent Jobs"),
                 const SizedBox(
                   height: 15,
                 ),
---
+++ lib/screen/homepage/widget/popular_row.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:job_app_ui/service/get_deta.dart';
+
+class PopularRow extends StatelessWidget {
+  const PopularRow({
+    super.key,
+    required this.data,
+    required this.size,
+  });
+
+  final GetData data;
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(data.getJobData().length, (index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: size.width * 0.75,
+              height: size.width * 0.4,
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade900,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        data.getJobData()[index].title,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      const Spacer(),
+                      Icon(
+                        Icons.book_online,
+                        color: Colors.grey.shade700,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        data.getJobData()[index].salary,
+                        style: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Text(
+                          data.getJobData()[index].type,
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      )
+                    ],
+                  ),
+                  const Spacer(),
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 25,
+                        backgroundImage:
+                            NetworkImage(data.getJobData()[index].logo),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            data.getJobData()[index].companyName,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            data.getJobData()[index].city,
+                            style: TextStyle(
+                              color: Colors.grey.shade200,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          )
+                        ],
+                      ),
+                      const Spacer(),
+                      Text(
+                        data.getJobData()[index].time,
+                        style: const TextStyle(
+                          color: Colors.red,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
