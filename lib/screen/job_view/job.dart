3c1921dc15f87014ce2b1a1ca30290017ae6552d
SalmanMohammed2001 <salmannisthar123@gmail.com>
2024-04-13 23:57:58 +0530

    JobDetails Banner Implementation

--- lib/screen/job_view/job.dart
+++ lib/screen/job_view/job.dart
@@ -20,6 +20,7 @@ class _JobViewPageState extends State<JobViewPage> {
             width: size.width,
             decoration: const BoxDecoration(
                 image: DecorationImage(
+              fit: BoxFit.cover,
               image: NetworkImage(
                   "https://freerangestock.com/sample/74590/job-interview--illustration-with-copyspace.jpg"),
             )),
@@ -39,6 +40,99 @@ class _JobViewPageState extends State<JobViewPage> {
               ],
             ),
           ),
+          Container(
+            width: size.width,
+            height: 150,
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  bottomRight: Radius.circular(15),
+                )),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Senior Software Enginnering",
+                    style: TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 2,
+                  ),
+                  Row(
+                    children: [
+                      const Text(
+                        "LKR 80000 Per Month",
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade300,
+                          borderRadius: BorderRadius.circular(9),
+                        ),
+                        child: const Text(
+                          "Full time",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      )
+                    ],
+                  ),
+                  const Spacer(),
+                  Row(
+                    children: [
+                      const CircleAvatar(
+                        radius: 20,
+                        backgroundImage: NetworkImage(
+                            "https://static.vecteezy.com/system/resources/previews/023/986/921/original/tiktok-logo-tiktok-logo-transparent-tiktok-icon-transparent-free-free-png.png"),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "TikTok",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "Colombo",
+                            style: TextStyle(
+                              color: Colors.grey.shade900,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          )
+                        ],
+                      ),
+                      const Spacer(),
+                      const Text(
+                        "4 Days Left",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
